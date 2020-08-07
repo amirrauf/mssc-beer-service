3692a57e681a9b3368f652e97a26a1010bf3af4b
Amir Rauf <arauf@carcrave.net>
2020-08-07 00:44:03 -0500

    adding beer repository closes #6

---
+++ src/main/java/guru/framework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package guru.framework.msscbeerservice.repositories;
+
+import guru.framework.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
