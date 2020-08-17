000bd12c65632801d10397ae311715315992747a
Amir Rauf <arauf@carcrave.net>
2020-08-17 13:03:42 -0500

    adding brewing service - closes #27

---
+++ src/main/java/guru/framework/msscbeerservice/services/BrewingService.java
@@ -0,0 +1,45 @@
+package guru.framework.msscbeerservice.services;
+
+import java.util.List;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import guru.framework.msscbeerservice.config.JmsConfig;
+import guru.framework.msscbeerservice.domain.Beer;
+import guru.framework.msscbeerservice.repositories.BeerRepository;
+import guru.framework.msscbeerservice.services.inventory.BeerInventoryService;
+import guru.framework.msscbeerservice.web.mappers.BeerMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BrewingService {
+    private final BeerRepository beerRepository;
+    private final BeerInventoryService beerInventoryService;
+    private final JmsTemplate jmsTemplate;
+    private final BeerMapper beerMapper;
+    
+    @Scheduled(fixedRate = 4000)
+    public void checkForLowInventory() {
+    	List<Beer> beers = beerRepository.findAll();
+    	
+    	beers.forEach(beer -> {
+    		Integer invQOH = beerInventoryService.getOnhandInventory(beer.getId());
+    		
+            log.debug("Min Onhand is: " + beer.getMinOnHand());
+            log.debug("Inventory is: "  + invQOH);
+            
+            if (beer.getMinOnHand() >= invQOH) {
+            	jmsTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, beerMapper.beerToBeerDto(beer));
+            }
+    		
+    	});
+    	
+    	
+    }
+
+}
