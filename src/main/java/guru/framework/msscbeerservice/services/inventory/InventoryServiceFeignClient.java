f3e5261ca30b95e69c9ebee7b2a01a4f0e13dd92
Amir Rauf <arauf@carcrave.net>
2020-08-25 14:57:23 -0500

    adding config for http basic with Feign Client

--- src/main/java/guru/framework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/guru/framework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import guru.framework.msscbeerservice.config.FeignClientConfig;
 import guru.framework.msscbeerservice.services.inventory.model.BeerInventoryDto;
 
-@FeignClient(name = "inventory-service", fallback = InventoryServiceFeignClientFailover.class)
+@FeignClient(name = "inventory-service", fallback = InventoryServiceFeignClientFailover.class, configuration = FeignClientConfig.class)
 public interface InventoryServiceFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
